c4b7bdfa44cb92fdac89a1991b597fe86b56544d
Anton Kulaga <19634898+antony-develop@users.noreply.github.com>
2018-09-29 19:34:46 +1000

    create class to work with weather data

---
+++ weather.js
@@ -0,0 +1,18 @@
+class Weather {
+    constructor(city, state) {
+        this.apiKey = '99dfe35fcb7de1ee';
+        this.city = city;
+        this.state = state;
+    }
+
+    async getWeather() {
+        const response = await fetch(`http://api.wunderground.com/api/${this.apiKey}/conditions/q/${this.state}/${this.city}.json`);
+        const data = await response.json();
+        return data.current_observation;
+    }
+
+    changeLocation(city, state) {
+        this.city = city;
+        this.state = state;
+    }
+}
